13909efd1988ceedf05cd11deecf3db5a2379daf
paulohenriquelima759-png <paulohenriquelima759@gmail.com>
2026-03-05 09:20:54 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,52 @@
+const celulares = [
+
+{
+nome:"Samsung A16",
+geral:95,
+reddot:90,
+dpi:420
+},
+
+{
+nome:"Samsung A12",
+geral:92,
+reddot:88,
+dpi:400
+},
+
+{
+nome:"Samsung A22",
+geral:94,
+reddot:90,
+dpi:410
+},
+
+{
+nome:"Redmi Note 11",
+geral:95,
+reddot:90,
+dpi:420
+},
+
+{
+nome:"Poco X3",
+geral:97,
+reddot:92,
+dpi:440
+},
+
+{
+nome:"Moto G60",
+geral:93,
+reddot:90,
+dpi:410
+},
+
+{
+nome:"iPhone 11",
+geral:98,
+reddot:93,
+dpi:450
+}
+
+]
